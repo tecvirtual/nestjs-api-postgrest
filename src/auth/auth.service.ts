75407bf5e4457ff61e1b59c67c707570bc71bf54
Frank Garcia <garcia1901l@gmail.com>
2023-08-26 23:57:22 -0500

    auth login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,43 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
+import * as bcryptjs from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
   async register({ name, email, password }: RegisterDto) {
     const user = await this.usersService.findOneByEmail(email);
     if (user) {
       throw new BadRequestException('User already exists');
     }
-    return await this.usersService.create({ name, email, password });
+    return await this.usersService.create({
+      name,
+      email,
+      password: await bcryptjs.hash(password, 10),
+    });
   }
 
-  login() {
-    return 'login';
+  async login({ email, password }: RegisterDto) {
+    const user = await this.usersService.findOneByEmail(email);
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+    const isMatch = await bcryptjs.compare(password, user.password);
+    if (!isMatch) {
+      throw new UnauthorizedException('Invalid password');
+    }
+    const payload = { email: user.email };
+
+    const token = this.jwtService.sign(payload);
+    return { token, email };
   }
 }
